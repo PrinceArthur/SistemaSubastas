937f49f4750199473cb817ceb2cf3eb2cbe2004f
PrinceArthur <42502801+PrinceArthur@users.noreply.github.com>
2018-11-11 11:56:14 -0500

    Update OfferersaleDAO.java

--- SistemaSubastas/src/dao/OfferersaleDAO.java
+++ SistemaSubastas/src/dao/OfferersaleDAO.java
@@ -7,7 +7,7 @@ import entity.Offerersale;
 public interface OfferersaleDAO
 {
 	public void nuevo(Offerersale offerersale);
-	public Offerersale getOfferersale(String userName);
+	public List<Offerersale> getOfferersale(String userName);
 	public void actualizar(Offerersale offerersale);
 	public void eliminar(Offerersale oferrersale);
 	public List<Offerersale> lista();
